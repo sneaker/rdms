6af810deaaa5e78ac5fca54dcde21c1726b86a47
Thomas Kallenberg <thomas@no-more-secrets.ch>
2014-11-23 16:51:22 +0100

    Corrected spelling mistake in validation

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,4 +1,4 @@
 class Document < ActiveRecord::Base
   validates :name, :description, :filename, :filetype, presence: true
-  validates :filesize, numericality: { greater_then_or_equal_to: 0 }
+  validates :filesize, numericality: { greater_than_or_equal_to: 0 }
 end
